63a54250c202901a696d2df69fa551978751f6f0
Pavel Tsarenko <breitsmiley@gmail.com>
2019-07-22 16:53:40 +0300

    fix dropping Authorization header

---
+++ web/1.php
@@ -0,0 +1,20 @@
+<?php
+
+//phpinfo();
+
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+echo "<pre>";
+foreach (getallheaders() as $name => $value) {
+    echo "$name: $value\n";
+}
+echo "</pre>";
